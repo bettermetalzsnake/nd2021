d5b5fa59ae0670db340e6dd1fe4825fb7692d2bd
bettermetalzsnake <marcmartinezemail@gmail.com>
2021-03-05 15:11:56 -0700

    moved files to have them all in one directiry

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -5,11 +5,13 @@ variable "prefix" {
 variable "location" {
   type = string
   description = "The Azure Region in which all resources in this example should be created."
+  default = westus2
 }
 
 variable "scalenum" {
   type = number
   description = "How many VMs would you like to deploy"
+  default = 1
 }
 
 variable "username" {
